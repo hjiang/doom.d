b89f0c1687d7d3e19a09bc68cbe5ed013c4c822b
Hong Jiang <j@1byte.io>
2020-12-13 23:42:08 +0800

    font: adjust linux font weight

--- config.el
+++ config.el
@@ -27,7 +27,7 @@
         (font-spec :family "FuraCode Nerd Font" :size 14 :weight 'light)))
  ((find-font (font-spec :family "Fira Code"))
   (setq doom-font
-        (font-spec :family "Fira Code" :size 14 :weight 'light))))
+        (font-spec :family "Fira Code" :size 14 :weight 'medium))))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
