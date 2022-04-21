f7f8afa8767490dcd7000de88205d863720e300e
Hong Jiang <j@1byte.io>
2022-04-21 10:50:02 +0800

    removed more modules

--- init.el
+++ init.el
@@ -129,16 +129,16 @@
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       (go +lsp)         ; the hipster dialect
-       ;;(haskell +dante)  ; a language that's lazier than I am
-       hy                ; readability of scheme w/ speed of python
-       ;;idris             ;
-       json              ; At least it ain't XML
+       ;; (go +lsp)         ; the hipster dialect
+       ;; (haskell +dante)  ; a language that's lazier than I am
+       ;; hy                ; readability of scheme w/ speed of python
+       ;; idris             ;
+       json                 ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       javascript          ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
-       ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+       ;; kotlin            ; a better, slicker Java(Script)
+       ;; latex            ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
