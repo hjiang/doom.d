8b316cf6bc2f8a394f6e1e716c16f2c3e7d6622e
Hong Jiang <lazyseq@gmail.com>
2023-04-24 19:07:33 +0800

    change theme to spacegrey

--- config.el
+++ config.el
@@ -58,7 +58,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
+(setq doom-theme 'doom-spacegrey)
 ;; (add-to-list 'custom-theme-load-path "~/.doom.d/themes")
 ;; (load-theme 'taptap t)
 
