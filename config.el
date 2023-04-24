0726f04924a1c122936843031176a22939cde35e
Hong Jiang <lazyseq@gmail.com>
2023-04-24 19:11:39 +0800

    remove roam-mode config

--- config.el
+++ config.el
@@ -137,30 +137,6 @@ is not the case"
   (c-mode-common . google-set-c-style)
   (c-mode-common . google-make-newline-indent))
 
-(setq org-roam-directory (file-truename "~/org/roam"))
-
-(when (file-directory-p org-roam-directory)
-  (use-package! org-roam
-    :init
-    (setq org-roam-v2-ack t)
-    :config
-    (org-roam-db-autosync-mode))
-
-  (use-package! websocket
-    :after org-roam)
-
-  (use-package! org-roam-ui
-    :after org-roam ;; or :after org
-    ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-    ;;         a hookable mode anymore, you're advised to pick something yourself
-    ;;         if you don't care about startup time, use
-    ;;  :hook (after-init . org-roam-ui-mode)
-    :config
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t)))
-
 ;; Org mode
 
 (defun orgfile (name)
