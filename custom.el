4077df2b61fdc1c163f6732735a537dedb57ee78
Hong Jiang <lazyseq@gmail.com>
2023-04-24 19:12:07 +0800

    add some theme whitelist

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" default))
  '(doom-one-brighter-comments t)
  '(doom-one-brighter-modeline t)
  '(package-selected-packages '(hy-mode)))
